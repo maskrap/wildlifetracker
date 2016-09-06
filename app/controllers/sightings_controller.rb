6081ad1cbc870e08c8ce9fca25f34ff82aaaaf1a
Cory Olson and Sam Park <cory+sam@>
2016-09-06 15:08:26 -0700

    fixed sighting addition form control'

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,2 +1,49 @@
 class SightingsController < ApplicationController
+  def new
+    @species = Species.find(params[:species_id])
+    @sighting = @species.sightings.new
+    @regions = Region.all
+
+  end
+
+  def create
+    @species = Species.find(params[:species_id])
+    @sighting = @species.sightings.new(sighting_params)
+    @region = @sightings.region.new(params[:regions_id])
+    if @sighting.save
+      redirect_to species_path(@sighting.species)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @species = Species.find(params[:id])
+    @sighting = Sighting.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @species = Species.find(params[:id])
+    @sighting = Sighting.find(params[:id])
+    if @sighting.update(sighting_params)
+      flash[:notice] = "Sighting updated!"
+      redirect_to species_path(@sighting.species)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @sighting = Sighting.find(params[:id])
+    @sighting.destroy
+    flash[:notice] = "Sighting deleted"
+    redirect_to species_path
+  end
+
+
+  private
+    def sighting_params
+      params.require(:sighting).permit(:description)
+    end
 end
